e0cc0e5dc528b50770dac015523526b403659e80
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2023-05-13 18:26:12 -0700

    still for state

--- models/state.py
+++ models/state.py
@@ -18,18 +18,3 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
-
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
