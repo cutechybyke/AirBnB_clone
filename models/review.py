36aa90a8971fc4fa305e5b8b7c28b9e63e6f7d0e
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2023-05-13 18:19:35 -0700

    for this

--- models/review.py
+++ models/review.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""For the review class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+"""This module for Review class."""
 
+from models.base_model import BaseModel
 
-class Review(BaseModel, Base):
-    """This Here, is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+class Review(BaseModel):
+    """For class representing a Review."""
+    place_id = ""
+    user_id = ""
+    text = ""
