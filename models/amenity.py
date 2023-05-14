b51d9e9b1756dd8a6ab225d12bf7aad170b06578
Obinna Nwaneri <chibuikeobinna3@gmail.com>
2023-05-13 18:04:15 -0700

    here

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""The amenity class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from models.place import place_amenity
+"""This module for Amenity class."""
+
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Here is the class for Amenity
-    Attributes:
-        Name: input Name
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+class Amenity(BaseModel):
+    """For class representing a Amenity."""
+    name = ""
